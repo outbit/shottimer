31673a4997689e4d3a20657cec9fbd0e666b7d81
David Whiteside <starboarder2001@gmail.com>
2015-07-19 00:11:47 -0600

    updated url to microchip compiler

--- travis-install-microchipc.sh
+++ travis-install-microchipc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-wget http://www.microchip.com/mplabxc8linux
-./xc8-*.run
+#wget http://www.microchip.com/mplabxc8linux
+wget http://ww1.microchip.com/downloads/en/DeviceDoc/xc8-v1.34-full-install-linux-installer.run
+./xc8-v1.34-full-install-linux-installer.run
