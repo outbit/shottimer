5ee8d28022fb0f33b71c9e7bdc6901a9e555c174
David Whiteside <starboarder2001@gmail.com>
2015-07-19 00:24:33 -0600

    getting microchip c compiler working, text mode

--- travis-install-microchipc.sh
+++ travis-install-microchipc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-sudo apt-get install libc6:i386 libx11-6:i386 libxext6:i386 libstdc++6:i386 libexpat1:i386
+#sudo apt-get install libc6:i386 libx11-6:i386 libxext6:i386 libstdc++6:i386 libexpat1:i386
 wget http://ww1.microchip.com/downloads/en/DeviceDoc/xc8-v1.34-full-install-linux-installer.run
 chmod 755 xc8-v1.34-full-install-linux-installer.run
 sudo ./xc8-v1.34-full-install-linux-installer.run --mode text
