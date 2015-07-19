e350bdef4df4a25c2d3f1aeedb8a17b4dec7f515
David Whiteside <starboarder2001@gmail.com>
2015-07-19 00:15:00 -0600

    getting microchip c compiler working

--- travis-install-microchipc.sh
+++ travis-install-microchipc.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#wget http://www.microchip.com/mplabxc8linux
 wget http://ww1.microchip.com/downloads/en/DeviceDoc/xc8-v1.34-full-install-linux-installer.run
 chmod a+rx ./xc8-v1.34-full-install-linux-installer.run
-./xc8-v1.34-full-install-linux-installer.run
+sudo ./xc8-v1.34-full-install-linux-installer.run
